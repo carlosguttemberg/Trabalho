10e7adc5c0bc1164e0d26877ba169f6d654b0b83
carlosguttemberg <guttembergcarlos@gmail.com>
2015-12-28 12:07:56 -0200

    Merge origin/master
    Conflicts:
    	web/Principal.jsp

--- src/java/controller/LivroController.java
+++ src/java/controller/LivroController.java
@@ -168,7 +168,7 @@ public class LivroController {
             retorno += "" +
             "<div class=\"col-sm-4 col-lg-4 col-md-4\">\n" +
             "   <div class=\"thumbnail\">" + 
-            "       <img src='"+livros.get(i).getCaminhofoto() + "' alt=''>" +
+            "       <img width=256px height=256px src='"+livros.get(i).getCaminhofoto() + "' alt=''>" +
                     /*
             "        <div class=\"caption\">  " +
             "           <h4 class=\"pull-right\">$64.99</h4>" + 
